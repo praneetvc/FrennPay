65e34f109a250a9487157595985007373db06109
Praneet Kumar Singh <praneet.singh@mail.vinove.com>
2022-03-29 15:44:59 +0530

    UI changes.

--- app/src/main/java/com/mfb/frennpay/MainActivity.java
+++ app/src/main/java/com/mfb/frennpay/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.mfb.frennpaylibrary.FrennPay;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.startButton).setOnClickListener(view -> {
+            FrennPay pay = new FrennPay();
+            pay.startFrennPay(this);
+        });
     }
 }
